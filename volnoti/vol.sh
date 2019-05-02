2d3cb5effacc0ef8edc90dece833d0dd90ce5d0f
Igor Laevsky <igmyrj@gmail.com>
2019-05-02 18:18:40 +0300

    Don't use fancy icons in volume control

--- volnoti/vol.sh
+++ volnoti/vol.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-icons="
- -0 /home/igor/.config/volnoti/icons/mute.svg
- -1 /home/igor/.config/volnoti/icons/off.svg
- -2 /home/igor/.config/volnoti/icons/down.svg
- -3 /home/igor/.config/volnoti/icons/down.svg
- -4 /home/igor/.config/volnoti/icons/up.svg"
+# icons="
+#  -0 /home/igor/.config/volnoti/icons/mute.svg
+#  -1 /home/igor/.config/volnoti/icons/off.svg
+#  -2 /home/igor/.config/volnoti/icons/down.svg
+#  -3 /home/igor/.config/volnoti/icons/down.svg
+#  -4 /home/igor/.config/volnoti/icons/up.svg"
 
 scale=2
 vol=$(($(pamixer --get-volume)/${scale}))
@@ -29,5 +29,5 @@ vol=$(($(pamixer --get-volume)/${scale}))
 if [[ $(pamixer --get-mute) == "true" ]]; then
   volnoti-show -m ${icons}
 else
-  volnoti-show ${icons} ${vol}
+  volnoti-show ${vol}
 fi
