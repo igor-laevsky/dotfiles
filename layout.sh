e909a0ea4a91d0e7445d04dcf2e09ec7b40b08dc
Igor Laevsky <igmyrj@gmail.com>
2019-06-15 17:26:57 +0300

    Switchtabs wit numbers, better multimonitor setup

--- layout.sh
+++ layout.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+export DISPLAY=":0"
+
 case "$1" in
   single)
-    xrandr --output HDMI-0 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.5 --filter nearest
+    xrandr --fb 1920x1080 --output HDMI-0 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.5 --filter nearest
+    xrandr --output DP-2 --panning 0x0+0+0
+    nitrogen --restore
+    #~/.config/polybar/launch.sh
     ;;
 
   dual-1200)
-    xrandr --fb 3840x1200 --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2 --mode 3840x2160 --pos 0x120 --rotate normal --scale 0.5 --filter nearest
+    xrandr --fb 3840x1200
+    xrandr --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal
+    xrandr --output DP-2 --panning 0x0+0+0
+    xrandr --output DP-2 --mode 3840x2160 --pos 0x120 --rotate normal --scale 0.5 --filter nearest
+    nitrogen --restore
+    #~/.config/polybar/launch.sh
     ;;
 esac
 
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar -l info mybar &
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar mybar &
+done
 
 echo "Bars launched..."
