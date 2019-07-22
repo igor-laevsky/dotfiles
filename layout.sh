02e0c76e618d21462f8cdbb6b05e15f830b7f719
Igor Laevsky <igor@azul.com>
2019-07-22 13:07:11 +0300

    Use nvidia-settings in dual monitor setup

--- layout.sh
+++ layout.sh
@@ -11,11 +11,17 @@ case "$1" in
     ;;
 
   dual-1200)
-    xrandr --output DP-2 --panning 0x0+0+0 
-    xrandr --output DP-2 --mode 3840x2160 --pos 0x120 --rotate normal --scale 0.5 --filter nearest
-    xrandr --fb 3840x1200
-    xrandr --output HDMI-0 --panning 0x0+0+0
-    xrandr  --fb 3840x1200 --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal
+    ## xrandr method is very unstable
+    # xrandr --output DP-2 --panning 0x0+0+0 
+    # xrandr --output DP-2 --mode 3840x2160 --pos 0x120 --rotate normal --scale 0.5 --filter nearest
+    # xrandr --fb 3840x1200
+    # xrandr --output HDMI-0 --panning 0x0+0+0
+    # xrandr  --fb 3840x1200 --output HDMI-0 --mode 1920x1200 --pos 1920x0 --rotate normal
+
+    nvidia-settings --assign "CurrentMetaMode=DPY-3: nvidia-auto-select @1920x1080 +0+120
+      {Transform=(0.500000,0.000000,0.000000,0.000000,0.500000,0.000000,0.000000,0.000000,1.000000), ViewPortIn=1920x1080, ViewPortOut=3840x2160+0+0, ResamplingMethod=Nearest}, DPY-2: nvidia-auto-select @1920x1200
+        +1920+0 {ViewPortIn=1920x1200, ViewPortOut=1920x1200+0+0}"    
+    
     nitrogen --restore
     #~/.config/polybar/launch.sh
     ;;
