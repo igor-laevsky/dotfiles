740fcfc742297b5c8a901913d61b06b58c606306
Igor Laevsky <igor.laevsky@jetbrains.com>
2021-06-29 20:04:29 +0300

    Some changes

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ ln -s ~/.config/.Xdefaults ~/.Xdefaults
 ln -s ~/.config/emacs/.emacs ~/.emacs
 ln -s ~/.config/gtk-2.0/gtkrc ~/.gtkrc-2.0
 
-ln -s ~/.config/.bash_profile ~/.bash_profile
 ln -s ~/.config/.bashrc ~/.bashrc
 ln -s ~/.config/.gitconfig ~/.gitconfig
 ln -s ~/.config/.xinitrc ~/.xinitrc
+ln -s ~/.config/.bash_profile ~/.bash_profile
 
 ln -s ~/.config/.zshrc ~/.zshrc
 ln -s ~/.config/.zprofile ~/.zprofile
